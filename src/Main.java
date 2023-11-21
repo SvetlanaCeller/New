693678c2d4a5aecbf90f9112253c4a580e76bbb7
Svetlana Celler <svetlanaglinskaa838@gmail.com>
2023-11-21 18:35:14 +0400

    gh

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Programmer {
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    private  String name;
+        private int age;
+    public  void WriteCode (String[] args) {
+
+    }
+
+}
